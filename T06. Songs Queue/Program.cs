c3fcbbb9bff794ee2b20822940cd6812367b0809
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-13 16:33:37 +0200

    T06, T07

---
+++ T06. Songs Queue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+Queue<string> songsQueue = new Queue<string>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries));
+
+while (songsQueue.Count > 0)
+{
+    string[] input = Console.ReadLine().Split(" ", 2);
+    string command = input[0];
+
+    switch (command)
+    {
+        case "Play":
+            songsQueue.Dequeue();
+            break;
+        case "Add":
+            string song = input[1];
+            if (songsQueue.Contains(song))
+            {
+                Console.WriteLine($"{song} is already contained!");
+            }
+            else
+            {
+                songsQueue.Enqueue(song);
+            }
+            break;
+        case "Show":
+            Console.WriteLine(string.Join(", ", songsQueue));
+            break;
+    }
+}
+Console.WriteLine("No more songs!");
---
+++ T07. Truck Tour/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Queue<PetrolStation> petrolStations = new Queue<PetrolStation>();
+int n = int.Parse(Console.ReadLine());
+int tank = 0;
+
+bool isSucceed = true;
+
+
+InitialiseStations(petrolStations, n);
+
+for (int i = 0; i < n; i++)
+{
+    foreach (var petrolStation in petrolStations)
+    {
+        if (petrolStation.PetrolAmount + tank <= petrolStation.Distance)
+        {
+            isSucceed = false;
+            break;
+        }
+        tank += petrolStation.PetrolAmount - petrolStation.Distance;
+    }
+
+    if (isSucceed)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    else
+    {
+        tank = 0;
+        var moveStation = petrolStations.Dequeue();
+        petrolStations.Enqueue(moveStation);
+        isSucceed = true;
+    }
+}
+
+
+
+void InitialiseStations(Queue<PetrolStation> queue , int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int amount = input[0];
+        int distance = input[1];
+
+        PetrolStation newStation = new PetrolStation(amount, distance);
+
+        queue.Enqueue(newStation);
+    }
+}
+
+
+class PetrolStation
+{
+    public PetrolStation(int amount, int distance)
+    {
+        PetrolAmount = amount;
+        Distance = distance;
+    }
+
+
+    public int PetrolAmount;
+    public int Distance;
+
+}
