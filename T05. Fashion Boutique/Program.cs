b9f357cd532c5b657e4ccc861c6b3b7dde4a50aa
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-13 13:30:09 +0200

    T5

---
+++ T05. Fashion Boutique/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Stack<int> clothesStack = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+int rackCapacity = int.Parse(Console.ReadLine());
+
+int rackUsed = 1;
+int arrangedClothes = 0;
+
+while (clothesStack.Any())
+{
+    int currentClothe = clothesStack.Peek();
+
+    if (arrangedClothes + currentClothe <= rackCapacity)
+    {
+        arrangedClothes += currentClothe;
+        clothesStack.Pop();
+    }
+    else
+    {
+        arrangedClothes = 0;
+        rackUsed ++;
+    }
+}
+
+Console.WriteLine(rackUsed);
