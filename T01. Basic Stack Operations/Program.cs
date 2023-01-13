1a4dcf7d9beb0605b3f3c2acb2f4969e970dd9d3
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-13 08:20:58 +0200

    Add project files.

---
+++ T01. Basic Stack Operations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            Stack<int> stack = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
---
+++ T02. Basic Queue Operations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            Queue<int> queue = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
