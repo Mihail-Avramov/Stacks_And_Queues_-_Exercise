46eda9918fdbd087365c5ae5b1bafb4e9f266314
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-13 09:32:04 +0200

    Adding T03 and T04

---
+++ T03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int queryCounts = int.Parse(Console.ReadLine());
+
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < queryCounts; i++)
+{
+    int[] inputArray = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+    int command = inputArray[0];
+
+    switch (command)
+    {
+        case 1:
+            int number = inputArray[1];
+            stack.Push(number);
+            break;
+        case 2:
+            if(stack.Any())
+                stack.Pop();
+            break;
+        case 3:
+            if(stack.Any())
+                Console.WriteLine(stack.Max());
+            break;
+        case 4:
+            if(stack.Any())
+                Console.WriteLine(stack.Min());
+            break;
+    }
+
+}
+
+Console.WriteLine(string.Join(", ", stack));
---
+++ T04. Fast Food/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int foodLeft = int.Parse(Console.ReadLine());
+Queue<int> ordersQueue = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+Console.WriteLine(ordersQueue.Max());
+
+while (ordersQueue.Any())
+{
+    int currentOrder = ordersQueue.Peek();
+
+    if (foodLeft < currentOrder)
+    {
+        Console.WriteLine($"Orders left: {string.Join(" ", ordersQueue)}");
+        break;
+    }
+
+    foodLeft -= currentOrder;
+    ordersQueue.Dequeue();
+}
+
+if (!ordersQueue.Any())
+{
+    Console.WriteLine("Orders complete");
+}
