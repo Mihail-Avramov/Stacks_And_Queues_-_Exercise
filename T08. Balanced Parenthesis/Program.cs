1407e2701414a75b8760987f6d71c262cf8799d8
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-13 17:13:44 +0200

    Т08 Т09

---
+++ T08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+string input = Console.ReadLine();
+Stack<char> openBrackets = new Stack<char>();
+
+foreach (var ch in input)
+{
+    switch (ch)
+    {
+        case '(':
+        case '{':
+        case '[':
+            openBrackets.Push(ch);
+            break;
+        case ')':
+            if (openBrackets.Any() && openBrackets.Peek() == '(')
+            {
+                openBrackets.Pop();
+                break;
+            }
+            Console.WriteLine("NO");
+            return;
+        case '}':
+            if (openBrackets.Any() && openBrackets.Peek() == '{')
+            {
+                openBrackets.Pop();
+                break;
+            }
+            Console.WriteLine("NO");
+            return;
+        case ']':
+            if (openBrackets.Any() && openBrackets.Peek() == '[')
+            {
+                openBrackets.Pop();
+                break;
+            }
+            Console.WriteLine("NO");
+            return;
+    }
+}
+
+if (openBrackets.Any())
+{
+    Console.WriteLine("NO");
+}
+else
+{
+    Console.WriteLine("YES");
+}
